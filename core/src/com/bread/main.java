d6860000e125a5937adbd8dc3fce06bbcb0f150f
bread <braddevans123@gmail.com>
2018-07-28 15:40:41 +0100

    THERESA BROKE THE ITTERATOR

---
+++ core/src/com/bread/main.java
@@ -0,0 +1,94 @@
+package com.bread;
+
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.PerspectiveCamera;
+import com.badlogic.gdx.graphics.VertexAttributes;
+import com.badlogic.gdx.graphics.VertexAttributes.Usage;
+import com.badlogic.gdx.graphics.g3d.Environment;
+import com.badlogic.gdx.graphics.g3d.Model;
+import com.badlogic.gdx.graphics.g3d.ModelBatch;
+import com.badlogic.gdx.graphics.g3d.ModelInstance;
+import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
+import com.badlogic.gdx.graphics.g3d.environment.DirectionalLight;
+import com.badlogic.gdx.graphics.g3d.Material;
+import com.badlogic.gdx.graphics.g3d.utils.CameraInputController;
+import com.badlogic.gdx.graphics.g3d.utils.ModelBuilder;
+import com.badlogic.gdx.utils.Array;
+
+public class main implements ApplicationListener {
+	public Environment environment;
+	public PerspectiveCamera cam;
+	public CameraInputController camController;
+	public ModelBatch modelBatch;
+	public Model model;
+	private Array<ModelInstance> instances = new Array<ModelInstance>();
+	public ModelInstance instance;
+
+	@Override
+	public void create() {
+		environment = new Environment();
+		environment.set(new ColorAttribute(ColorAttribute.AmbientLight, 0.4f, 0.4f, 0.4f, 1f));
+		environment.add(new DirectionalLight().set(0.8f, 0.8f, 0.8f, -1f, -0.8f, -0.2f));
+		
+		modelBatch = new ModelBatch();
+		
+		cam = new PerspectiveCamera(67, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		cam.position.set(10f, 10f, 10f);
+		cam.lookAt(0,0,0);
+		cam.near = 1f;
+		cam.far = 300f;
+		cam.update();
+
+        ModelBuilder modelBuilder = new ModelBuilder();
+        model = modelBuilder.createBox(2f, 2f, 2f,
+                new Material(),
+                VertexAttributes.Usage.Position |
+                        VertexAttributes.Usage.Normal |
+                        VertexAttributes.Usage.TextureCoordinates);
+
+        instances = new ModelInstance(model);
+
+		for (int x = -5; x <= 5; x+=2) {
+			for (int z = -5; z <= 5; z+=2) {
+				instances.add(new ModelInstance(model, x, 10f, z));
+			}
+		}
+        
+        camController = new CameraInputController(cam);
+        Gdx.input.setInputProcessor(camController);
+	}
+
+	@Override
+	public void render() {
+		camController.update();
+		
+        Gdx.gl.glViewport(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
+ 
+        modelBatch.begin(cam);
+        modelBatch.render(instance, environment);
+        modelBatch.end();
+	}
+	
+	@Override
+	public void dispose() {
+		modelBatch.dispose();
+		model.dispose();
+	}
+
+	@Override
+	public void resize(int width, int height) {
+	}
+
+	@Override
+	public void pause() {
+	}
+
+	@Override
+	public void resume() {
+	}
+}
---
+++ desktop/src/com/bread/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.bread.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.bread.main;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new main(), config);
+	}
+}
